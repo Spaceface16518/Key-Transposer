7e78e77bd2110e16bb35c558499061861a6c47a1
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2017-12-11 14:53:54 -0600

    Update transpose.js

--- .gitignore/transpose.js
+++ .gitignore/transpose.js
@@ -1,5 +1,17 @@
-
+var keyOffset;
+var newNote;
+function setKey(key) {
+  switch (key) {
+    case 'A':
+      keyOffset = -3
+      break;
+    case 'B flat':
+    case 'Bb':
+      keyOffset = -2
+      break;
+  }
+}
 
 function transpose(note) {
-
+newNote = note + keyOffset;
 }
