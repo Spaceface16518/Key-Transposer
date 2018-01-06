de496428c1ba9f15dc48ff7f05dcd630487dabb8
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-01-06 17:01:32 -0600

    Added onload function(s)

---
+++ main.js
@@ -0,0 +1,5 @@
+// onload functions are wrapped in a document ready jquery statement
+$(document).ready(function () {
+  let pageSource = './modules/dropdown.html';
+  $("#dropdown").html(); // TODO: add a way for the page above to be recited in this function
+});
