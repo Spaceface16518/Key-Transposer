4582da18be08b538ef697243f9734c436e0d572c
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-01-06 20:28:17 -0600

    Added experimental dynamic stylesheet
    Note: Support for calling the function is already in the main file
    Note: This stylesheet only has styles for * and body

---
+++ modules/dynamic-style.jquery.js
@@ -0,0 +1,12 @@
+function setDynamicStyle() {
+  $("*").css({
+    "box-sizing": "border-box"
+  });
+  $("body").css({
+    "font-family": "'Helvetica Neue', Helvetica, sans-serif",
+    "font-size": "13px",
+    "background-color": "rgb(84, 228, 168)",
+    "text-align": "center"
+  });
+  // This is not all of the the css page, just enough to test it out
+}
