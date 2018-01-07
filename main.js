abcc3fc5211ae01ec5f1c4c53bbbe5d67281c632
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-01-06 20:23:39 -0600

    Added style function call

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 // onload functions are wrapped in a document ready jquery statement
-$(document).ready(function () {
+$(document).ready(function() {
   let pageSource = './modules/dropdown.html';
   $("#dropdown").html(); // TODO: add a way for the page above to be recited in this function
+
+  setDynamicStyle() // Experimental JQuery style function
 });
