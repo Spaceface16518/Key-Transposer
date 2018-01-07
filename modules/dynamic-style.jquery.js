743e6919c53fb041dcff0ea15f149b0211ae6059
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-01-07 00:04:41 -0600

    Added rest of dynamic styles

--- modules/dynamic-style.jquery.js
+++ modules/dynamic-style.jquery.js
@@ -2,11 +2,26 @@ function setDynamicStyle() {
   $("*").css({
     "box-sizing": "border-box"
   });
+
   $("body").css({
     "font-family": "'Helvetica Neue', Helvetica, sans-serif",
     "font-size": "13px",
     "background-color": "rgb(84, 228, 168)",
     "text-align": "center"
   });
-  // This is not all of the the css page, just enough to test it out
+
+  $("#result").css({
+    "color": "rgb(4, 53, 95)",
+    "font-size": "100%"
+  });
+
+  $("#result :hover").css({
+    "font-size": "110%",
+    "text-shadow": "4px"
+  });
+
+  $("h3").css({
+    "font-family": "'Trebuchet MS', Arial, sans-serif",
+    "font-size": "120%"
+  })
 }
