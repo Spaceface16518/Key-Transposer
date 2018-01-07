5ab68fa3eb3e3652cf955e270fe680d2c82e3669
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-01-07 00:31:15 -0600

    Added import statements
    - imported dynamic stylesheet
    - imported transpose
    - imported write
    
    Note: for some reason, the dynamic stylesheet is not working. This leads me to believe that the other modules are not working. Will have to fix.

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+import * as DynamicStyle from "./modules/dynamic-style.jquery.js";
+import * as Transpose from "./modules/transpose.js";
+import * as Write from "./modules/write.js"
 // onload functions are wrapped in a document ready jquery statement
 $(document).ready(function() {
   let pageSource = './modules/dropdown.html';
