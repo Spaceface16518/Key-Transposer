78554b16aebf51c7ce0465ee720b7f2ad823cdbd
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-01-07 22:40:53 -0600

    Write module
    - initial try at making the write module
    Note: this should be the last commit before the modular branch is updated.

--- modules/write.js
+++ modules/write.js
@@ -3,5 +3,5 @@ const Write = {}
 
 
 Write.write = function (text) {
-  //set variable to returned text, then write that variable onto the document using DOM, html, or jquery.
+$("#result").text(text);
 }
