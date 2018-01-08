728337a70ed78465685cbfad224fadfa860c1ccf
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-01-07 22:34:17 -0600

    Correct method call (Dynamic Style)

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
   let pageSource = './modules/dropdown.html';
   $("#dropdown").html(); // TODO: add a way for the page above to be recited in this function
 
-  setDynamicStyle() // Experimental JQuery style function
+  DynamicStyle.setDynamicStyle() // Experimental JQuery style function
 });
 
 $("#submit").clicked(function () {
