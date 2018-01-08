299ecc45a47400c77b9111389a20e66c527834ff
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-01-07 21:46:51 -0600

    Receive functions
    - added support for receive functions
    - added skeletons for receive functions
    Note: the actual functions still need to be written in

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 // onload functions are wrapped in a document ready jquery statement
-$(document).ready(function() {
+$(document).ready(function () {
   let pageSource = './modules/dropdown.html';
   $("#dropdown").html(); // TODO: add a way for the page above to be recited in this function
 
   setDynamicStyle() // Experimental JQuery style function
+});
+
+$("#submit").clicked(function () {
+
+  let note = Receive.receiveNote;
+  let key = Receive.receiveKey;
+  let newNote = Transpose.transpose(note, key)
+  console.log(newNote);
+  Write.write(newNote);
+
 });
